6c36cf8c2c546e42ba99c21224afd19fc2345058
asus <755366307@qq.com>
2020-04-27 14:46:53 +0800

    要提交的代码

---
+++ app/src/main/java/com/example/sharetest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.sharetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //李洪写的信息
+    }
+}
